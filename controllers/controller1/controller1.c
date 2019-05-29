d9d38c568a58780bcaa9cd8af8ae0fa0f255f72e
Shadeenz <al1701002@upy.edu.mx>
2019-05-29 08:12:35 -0500

    LED turn on and off complete

--- controllers/controller1/controller1.c
+++ controllers/controller1/controller1.c
@@ -11,8 +11,10 @@
  * <webots/differential_wheels.h>, etc.
  */
 #include <webots/robot.h>
+#include <webots/led.h>
 
 #include <stdio.h>
+
 
 
 /*
@@ -29,7 +31,11 @@
 int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
-  wb_robot_init();
+ wb_robot_init();
+   printf("Doing some basic setup\n");
+
+  int seconds_counter=0;
+  int led_state;
 
   /*
    * You should declare here WbDeviceTag variables for storing
@@ -37,9 +43,9 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
-printf("Doing some basic setup\n");
 
-int seconds_counter=0;
+
+  WbDeviceTag myled = wb_robot_get_device("led");
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
@@ -47,9 +53,19 @@ int seconds_counter=0;
   while (wb_robot_step(TIME_STEP) != -1) {
   
   printf("Doing some repetitive task\n");
+  
   printf("Time = %i\n", seconds_counter);
   seconds_counter ++;
-  
+
+
+  led_state = wb_led_get(myled);    
+  printf("LED Value is %i\n", led_state);
+
+
+ 
+   
+   
+
 
     /*
      * Read the sensors :
@@ -63,6 +79,15 @@ int seconds_counter=0;
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
+
+  
+  if (led_state== 1){
+  wb_led_set(myled, 0);
+  }
+  else  wb_led_set(myled, 1);
+  
+   
+     
   };
 
   /* Enter your cleanup code here */
